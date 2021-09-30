930bf30fbb26266c362e4e62c0e8fd2028d19531
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-09-30 09:39:44 +0300

    add app Layout component

---
+++ components/_App/Layout.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+import Header from "./Header";
+import HeadContent from "./HeadContent";
+
+function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <HeadContent />
+        <title>ReactReserve</title>
+      </Head>
+      <Header />
+      <Container>{children}</Container>
+    </>
+  );
+}
+
+export default Layout;
+
+function Container({ children }) {
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+  );
+}
