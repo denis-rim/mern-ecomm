b4ac0d24b9537314b752a0827bb4d6a10f2298ca
Denis Mazurchuk <81959108+denis-rim@users.noreply.github.com>
2021-10-01 13:36:59 +0300

    Delete next.config.js
    Security issue

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://darktree:Dude340340@mern-db.dua47.mongodb.net/mern-db?retryWrites=true&w=majority",
-  },
-};
