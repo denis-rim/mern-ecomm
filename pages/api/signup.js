6eb23a4a44dd7d46bdea21d969e9a5d276623731
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-10-12 17:43:55 +0300

    add singup validate data

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,7 +1,8 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import isEmail from "validator/lib/isEmail";
+import isLength from "validator/lib/isLength";
 import User from "../../models/User";
-import { handleLogin } from "../../utils/auth";
 import connectDb from "../../utils/connectDB";
 
 connectDb();
@@ -9,11 +10,23 @@ connectDb();
 export default async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    if (!isLength(name, { min: 3, max: 15 })) {
+      return res.status(422).send("Name must be 3-15 characters long");
+    } else if (!isLength(password, { min: 6 })) {
+      return res
+        .status(422)
+        .send("Password must be at least 6 characters long");
+    } else if (!isEmail(email)) {
+      return res.status(422).send("Email must be valid");
+    }
+
     const user = await User.findOne({ email });
+
     if (user) {
-      res.status(422).send(`User already exist with email ${email}.`);
+      return res.status(422).send(`User already exist with email ${email}.`);
     }
     const hash = await bcrypt.hash(password, 10);
+
     const newUser = await new User({
       name,
       email,
@@ -24,9 +37,9 @@ export default async (req, res) => {
       expiresIn: "30d",
     });
 
-    res.status(201).json(token);
+    return res.status(201).json(token);
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error signup user. Please try again later.");
+    return res.status(500).send("Error signup user. Please try again later.");
   }
 };
