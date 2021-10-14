d911bd5c91961536f2cdfff12bebf73d18f3d847
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-10-14 11:26:48 +0300

    cart api, model

--- pages/cart.js
+++ pages/cart.js
@@ -1,13 +1,29 @@
+import axios from "axios";
+import { parseCookies } from "nookies";
 import CartItemList from "../components/Cart/CartItemList";
 import CartSummary from "../components/Cart/CartSummary";
+import baseUrl from "../utils/baseUrl";
 
-function Cart() {
+function Cart({ products }) {
   return (
     <>
-      <CartItemList />
+      <CartItemList products={products} />
       <CartSummary />
     </>
   );
 }
+
+Cart.getInitialProps = async (ctx) => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { products: [] };
+  }
+
+  const response = await axios.get(`${baseUrl}/api/cart`, {
+    headers: { Authorization: token },
+  });
+
+  return { products: response.data };
+};
 
 export default Cart;
