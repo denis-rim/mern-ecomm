5b6fd30dac06a833a91ecd34ce3d53499da7359a
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-10-26 17:12:42 +0300

    add prod addres

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://deployment-url.com"
+    ? "reactnextshop-ar.now.sh"
     : "http://localhost:3000";
 
 export default baseUrl;
