b6c9cbe491f10e5c6ee0da70eac07643d618f611
Denis Mazurchuk <denis.aurorim@gmail.com>
2021-10-26 17:34:34 +0300

    remove slash from address again

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://mern-ecomm-85zhuu9bz-denis-rim.vercel.app"
+    ? "https://mern-ecomm.vercel.app/"
     : "http://localhost:3000";
 
 export default baseUrl;
